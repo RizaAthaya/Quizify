b2306a93eb1252767a612b0a12e9798399cea47f
RizaAthaya <athayarizaaa@gmail.com>
2024-12-28 18:35:59 +0700

    fix(deployment): compatibilty issue)

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -2,8 +2,10 @@ import forge from 'node-forge';
 
 const secretKey = import.meta.env.VITE_ENCRYPT_KEY;
 
+const keyBytes = forge.util.createBuffer(secretKey, 'utf8'); 
+
 export const handleEncrypt = (data: Record<string, any>) => {
-    const cipher = forge.cipher.createCipher('AES-CBC', secretKey);
+    const cipher = forge.cipher.createCipher('AES-CBC', keyBytes);
     const iv = forge.random.getBytesSync(16); // AES 
     cipher.start({ iv });
     cipher.update(forge.util.createBuffer(JSON.stringify(data)));
@@ -18,7 +20,7 @@ export const handleDecrypt = (data: string) => {
         const iv = encryptedBytes.slice(0, 16);
         const encryptedData = encryptedBytes.slice(16);
 
-        const decipher = forge.cipher.createDecipher('AES-CBC', secretKey);
+        const decipher = forge.cipher.createDecipher('AES-CBC', keyBytes);
         decipher.start({ iv });
         decipher.update(forge.util.createBuffer(encryptedData));
         decipher.finish();
@@ -26,6 +28,7 @@ export const handleDecrypt = (data: string) => {
         const decrypted = decipher.output.toString();
         return JSON.parse(decrypted);
     } catch (error) {
+        console.error('Decryption error:', error);
         return {
             token: undefined,
             num: 0,
